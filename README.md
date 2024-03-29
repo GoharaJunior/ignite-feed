c039404ed3f298f0bf84a53b8ab96b10ada4f0ce
Gohara <gohara.dev@gmail.com>
2024-03-29 10:49:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">
+    <br>
+        <img src=".github/capa.png" alt="Logo King Host">
+    <br>
+</h1>
+<p align="center"> Visualize o projeto:
+
+</p>
+
+<!-- <img src=".github/demo.gif" alt="Demonstração do Projeto" height="350">&nbsp;&nbsp;
+<img src=".github/demo-mobile.gif" alt="Demonstração do Projeto" height="350">
+<br>
+<br> -->
+
+<!-- ## ⚡ Tecnologias
+
+<img alt="HTML" src="https://img.shields.io/badge/HTML5-E34F26?logo=html5&logoColor=fff&style=flat">&nbsp;
+<img alt="CSS" src="https://img.shields.io/badge/CSS3-1572B6?logo=css3&logoColor=fff&style=flat">&nbsp;
+<img alt="sass" src="https://img.shields.io/badge/Sass-hotpink.svg?logo=SASS&logoColor=white">&nbsp;
+<img alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?logo=javascript&logoColor=black">&nbsp;
+<img alt="Gulp" src="https://img.shields.io/badge/gulp-CF4647?logo=gulp&logoColor=fff&style=flat">&nbsp; -->
+
+<!-- ## ℹ️ Como Usar
+
+
+#### ⚙️ Requisitos
+
+<img alt="Git" src="https://img.shields.io/badge/Git-F05032?logo=git&logoColor=fff&style=flat">&nbsp;
+<img alt="Gulp" src="https://img.shields.io/badge/gulp-CF4647?logo=gulp&logoColor=fff&style=flat">&nbsp;
+<img alt="Node Js" src="https://img.shields.io/badge/Node.js-393?logo=nodedotjs&logoColor=fff&style=flat">&nbsp; -->
+
+
+#### Iniciando o Desenvolvimento
+
+```sh
+# Instalar dependências:
+npm install
+
+# Iniciando a aplicação 
+npm run dev
+```
+
+<p align="LEFT"><kbd><b>
+    <a href="#top">VOLTE PARA O TOPO 🔝</a>
+</b></kbd></p> 
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar 
+                hasBorder={false}
+                src="https://github.com/diego3g.png"
+                alt=""
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndtime}>
+                            <strong>Diego Fernandes</strong>
+                            <time title="25 de março às 22:30" dateTime="2024-03-25 22:20:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
